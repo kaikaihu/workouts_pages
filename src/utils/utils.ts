3f02ef83bfb92da6a1bac3f4affb1ce18a19e421
KAIKAIHU <65703414+kaikaihu@users.noreply.github.com>
2024-01-24 18:03:21 +0800

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -257,14 +257,14 @@ const typeForRun = (run: Activity): string => {
         return 'Half Marathon';
       }
       return 'Trail Run';
-    // case 'Track Run':
-    //   if (distance >= 40) {
-    //     return 'Full Marathon';
-    //   }
-    //   else if (distance > 20) {
-    //     return 'Half Marathon';
-    //   }
-    //   return 'Track Run';
+    case 'Track Run':
+      if (distance >= 40) {
+         return 'Full Marathon';
+       }
+       else if (distance > 20) {
+         return 'Half Marathon';
+       }
+       return 'Track Run';
     case 'Ride':
       return 'Ride';
     case 'Indoor Ride':
